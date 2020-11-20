a67f2948245184818a81458894e8438bf32091b8
Prajwal-Panda <73119814+Prajwal-Panda@users.noreply.github.com>
2020-11-20 18:29:58 +0530

    Add files via upload

---
+++ Delete.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+ 
+
+int main()
+
+{
+
+    FILE *fileptr1, *fileptr2;
+
+    
+    char *filename = "library.txt";
+
+    char ch;
+
+    int delete_line, temp = 1;
+
+ 
+
+    
+
+    //open file in read mode
+    
+
+    fileptr1 = fopen(filename, "r");
+
+    ch = getc(fileptr1);
+
+    while (ch != EOF)
+
+    {
+
+        printf("%c", ch);
+
+        ch = getc(fileptr1);
+
+    }
+
+    //rewind
+
+    rewind(fileptr1);
+
+    printf(" \n Enter the row of the book to be removed:");
+
+    scanf("%d", &delete_line);
+
+    //open new file in write mode
+
+    fileptr2 = fopen("replica.c", "w");
+
+    ch = getc(fileptr1);
+
+    while (ch != EOF)
+
+    {
+
+        ch = getc(fileptr1);
+
+        if (ch == '\n')
+
+            temp++;
+
+            //except the line to be deleted
+
+            if (temp != delete_line)
+
+            {
+
+                //copy all lines in file replica.c
+
+                putc(ch, fileptr2);
+
+            }
+
+    }
+
+    fclose(fileptr1);
+
+    fclose(fileptr2);
+
+    remove(filename);
+
+    //rename the file replica.c to original name
+
+    rename("replica.c", filename);
+
+    printf("\n The Updated list of books are as follows:\n");
+
+    fileptr1 = fopen(filename, "r");
+
+    ch = getc(fileptr1);
+
+    while (ch != EOF)
+
+    {
+
+        printf("%c", ch);
+
+        ch = getc(fileptr1);
+
+    }
+
+    fclose(fileptr1);
+
+    return 0;
+
+}
