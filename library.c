3c31febb79a6f9df113b34d431333a2b6c7a6db4
Agnibha Banerjee <riobanerjee@gmail.com>
2020-11-21 20:29:27 +0530

    Everything works. Except updating in file when book issue/return

--- library.c
+++ library.c
@@ -7,6 +7,9 @@
 #define N_ALLOWED_F 6
 #define N_ALLOWED_S 3
 
+static int books_in_lib;
+static int users_in_lib;
+
 struct book
 {
     char book_id[10];
@@ -54,6 +57,7 @@ void initialize()
         token = strtok(NULL, ";");
         i++;
     }
+    books_in_lib = i;
     fclose(fp);
 
     i = 0;
@@ -81,13 +85,54 @@ void initialize()
         token = strtok(NULL, ";");
         i++;
     }
+    users_in_lib = i;
+}
+
+// Returns position at which book is at
+int which_book(char bookid[])
+{
+    int pos = -1;
+    // searching for the position the requested book is located at
+    for (int i = 0; i < books_in_lib; i++)
+    {
+        if (strcmp(bookid,books[i].book_id)==0)
+        {
+            pos = i;
+            break;
+        }
+    }
+    if (pos==-1)
+    {
+        printf("\nBook not found\n");
+    }
+    return(pos);
+}
+
+// Returns position at which user is at
+int which_user(char userid[])
+{
+    int pos = -1;
+    // searching for the position the requested book is located at
+    for (int i = 0; i < users_in_lib; i++)
+    {
+        if (strcmp(userid,users[i].user_id)==0)
+        {
+            pos = i;
+            break;
+        }
+    }
+    if (pos==-1)
+    {
+        printf("\nUser not found\n");
+    }
+    return(pos);
 }
 
 // gets number of copies of book x still in library
 int n_available(struct book x)
 {
     int count = 0;
-    for(int i = 0; i < N_USERS; i++)
+    for(int i = 0; i < users_in_lib; i++)
     {
         for(int j = 0; j < N_ALLOWED_F; j++)
         {
@@ -145,21 +190,33 @@ int hasbook(struct user x, struct book y)
 }
 
 // book return
-struct user book_return(struct user x, struct book y)
+void book_return(struct user x, struct book y)
 {
+    int pos = -1;
     for(int i = 0; i < N_ALLOWED_F; i++)
     {
         if(strcmp(x.issued[i],y.book_id)==0)
         {
-            strcpy(x.issued[i],"\0");
-            //add something to check if late return
+            pos = i;
         }
     }
-    return(x);
+    if (pos==-1)
+    {
+        printf("\n You have not issued this book.\n");
+    }
+    else
+    {
+        for(int i = pos; i < N_ALLOWED_F-1; i++)
+        {
+            strcpy(users[which_user(x.user_id)].issued[i],users[which_user(x.user_id)].issued[i+1]);
+        }
+        printf("\nBook has been returned.\n");
+    }
+    
 }
 
 //book issue
-struct user book_issue(struct user x, struct book y)
+void book_issue(struct user x, struct book y)
 {
     int status = 1;
     int days;
@@ -199,56 +256,18 @@ struct user book_issue(struct user x, struct book y)
 
     if(status == 1)
     {
-       days = available_days(y);
-       strcpy(x.issued[n_issued(x)],y.book_id);
-       printf("Book succesfully issued.\n");
+        days = available_days(y);
+        strcpy(users[which_user(x.user_id)].issued[n_issued(x)],y.book_id);
+        printf("Book succesfully issued.\n");
+        printf("You have issued it for %d days\n",days);
     }
     else
     {
         printf("%s",err_msg);
     }
-    return(x);
 }
 
-// Returns position at which book is at
-int which_book(char bookid[])
-{
-    int pos = -1;
-    // searching for the position the requested book is located at
-    for (int i = 0; i < N_BOOKS; i++)
-    {
-        if (strcmp(bookid,books[i].book_id)==0)
-        {
-            pos = i;
-            break;
-        }
-    }
-    if (pos==-1)
-    {
-        printf("\nBook not found\n");
-    }
-    return(pos);
-}
 
-// Returns position at which user is at
-int which_user(char userid[])
-{
-    int pos = -1;
-    // searching for the position the requested book is located at
-    for (int i = 0; i < N_USERS; i++)
-    {
-        if (strcmp(userid,users[i].user_id)==0)
-        {
-            pos = i;
-            break;
-        }
-    }
-    if (pos==-1)
-    {
-        printf("\nUser not found\n");
-    }
-    return(pos);
-}
 //===================================================AGNI END================================================================
 
 //=================================================PANDA START=============================================================
@@ -273,17 +292,8 @@ void readFile(FILE * fP)
 
 void add_book()
 {
-    int pos = 0;
-    //get number of books already in records
-    for(int i = 0; i < N_BOOKS; i++)
-    {
-        if (books[i].book_id==NULL)
-        {
-            pos = i;
-            break;
-        }
-    }
-    if (pos == N_BOOKS-1)
+    int pos = books_in_lib;
+    if (pos == N_BOOKS)
     {
         printf("\nLibrary is Full.\n");
         return;
@@ -295,39 +305,44 @@ void add_book()
     scanf("%s",str);
 
     char* token = strtok(str, ";");
-    strcpy(books[pos+1].book_id,token);
+    strcpy(books[pos].book_id,token);
     token = strtok(NULL, ";");
-    strcpy(books[pos+1].title,token);
+    strcpy(books[pos].title,token);
     token = strtok(NULL, ";");
-    strcpy(books[pos+1].auth,token);
+    strcpy(books[pos].auth,token);
     token = strtok(NULL, ";");
-    strcpy(books[pos+1].publ,token);
+    strcpy(books[pos].publ,token);
     token = strtok(NULL, ";");
-    books[pos+1].n_copies = atoi(token);
+    books[pos].n_copies = atoi(token);
 
+    books_in_lib++;
     /* Print file contents after appending string */
     printf("\nSuccessfully added the Book to the Library. \n");
 }
 
 void delete_book()
 {
+    int pos = -1;
     printf("Enter book id to be deleted\n");
     char bookid[10];
     scanf("%s",bookid);
-    int pos = which_book(bookid);
+    pos = which_book(bookid);
+    if(pos==-1)
+    {
+        printf("Book not found\n");
+        return;
+    }
     //deleting the book from our records
     //this is done by shifting all elements to the left from the required position 
-    for (int i = pos; i < N_BOOKS-1; i++)
+    for (int i = pos; i < books_in_lib-1; i++)
     {
-        if (books[i].book_id!=NULL)
-        {
-            strcpy(books[i].book_id,books[i+1].book_id);
-            strcpy(books[i].title,books[i+1].title);
-            strcpy(books[i].auth,books[i+1].auth);
-            strcpy(books[i].publ,books[i+1].publ);
-            books[i].n_copies = books[i+1].n_copies;
-        }
+        strcpy(books[i].book_id,books[i+1].book_id);
+        strcpy(books[i].title,books[i+1].title);
+        strcpy(books[i].auth,books[i+1].auth);
+        strcpy(books[i].publ,books[i+1].publ);
+        books[i].n_copies = books[i+1].n_copies;
     }
+    books_in_lib--;
 
 }
 
@@ -346,49 +361,37 @@ void update_records()
         return;
     }
     int i = 0;
-    fprintf(fp,"ID;Title;Author;Publisher;Number\n");
 
-    while (books[i].book_id != NULL)
+    while (i<books_in_lib)
     {
         fprintf(fp,"%s;%s;%s;%s;%d\n",books[i].book_id,books[i].title,books[i].auth,books[i].publ,books[i].n_copies);
         i++;
     }
     fclose(fp);
-    printf("Updated list of Books:\n\n");
-    readFile(fp);
-
-    /* Done with file, hence close file. */
-    fclose(fp);
 
     i = 0;
     int j = 0;
-    fp = fopen(filename2, "w");
-    fprintf(fp,"ID;Issued\n");
+    //USERS NOT  GETTING UPDATED
+    // fp = fopen(filename2, "w");
 
-    if (fp == NULL)
-    {
-        printf("Could not open file %s",filename2);
-        return;
-    }
-    while (users[i].user_id != NULL)
-    {
-        fprintf(fp,"%s;",users[i].user_id);
-        j = 0;
-        while(users[i].issued[j] != NULL)
-        {
-            fprintf(fp,"%s|",users[i].issued[j]);
-            j++;
-        }
-        fprintf(fp,"\n");
-        i++;
-    }
+    // if (fp == NULL)
+    // {
+    //     printf("Could not open file %s",filename2);
+    //     return;
+    // }
+    // while (i<users_in_lib)
+    // {
+    //     fprintf(fp,"%s;",users[i].user_id);
+    //     j = 0;
+    //     while(users[i].issued[j]!=NULL)
+    //     {
+    //         fprintf(fp,"%s|",users[i].issued[j]);
+    //         j++;
+    //     }
+    //     fprintf(fp,"\n");
+    //     i++;
+    // }
 
-    fclose(fp);
-
-    printf("Updated list of Users:\n\n");
-    readFile(fp);
-
-    /* Done with file, hence close file. */
     fclose(fp);
 }
 //==================================================PANDA END==============================================================
@@ -406,7 +409,7 @@ int login(char userid[]) //to check against list the input username (0 for not f
 {
     
     int var_login = 0; //binary variable that decides if login happens or not, default is logged out
-    for(int i = 0; i < N_USERS; i++)
+    for(int i = 0; i < users_in_lib; i++)
     {
         if(strcmp(userid,users[i].user_id)==0) // checks id the username and user id are same,if same login   
         {  
@@ -441,7 +444,6 @@ void search()
 void main()
 {
     initialize();
-
     char option_ad , user_id[10] , choice;
     int login_access = 0;
 	
@@ -450,8 +452,10 @@ void main()
 	printf("1 : Log In\n");
 	printf("2 : Search for a book\n");
 	printf("Enter your choice : ");
-	scanf("%c" , choice);
-	switch(choice){
+	scanf(" %c" , &choice);
+
+	switch(choice)
+    {
 	case '1' : 
     	while(login_access==0)
     	{
@@ -475,13 +479,14 @@ void main()
     	            printf("1 : Issue Book\n"); 
     	            printf("2 : Return Book\n");
     	            printf("3 : Search Book\n");
-    	            printf("Enter your choice : ");
+    	            printf("Enter your choice : \n");
     	            scanf(" %c" , &option_ad);
     	            switch(option_ad)
     	            {
     	                case '1' :
     	                    printf("Issue book\n");
     	                    printf("Enter Books ID of book to be issued\n");
+                            char book_id[10];
     	                    scanf(" %s" , book_id);
     	                    book_pos = which_book(book_id);
     	                    book_issue(users[user_pos],books[book_pos]);
@@ -509,7 +514,7 @@ void main()
     	            printf("2 : Delete Book\n");
     	            printf("3 : Edit Book\n");
     	            printf("4 : Search Book\n");
-    	            printf("Enter your choice : ");
+    	            printf("Enter your choice : \n");
     	            scanf(" %c" , &option_ad);
     	            switch(option_ad)
     	            {
@@ -540,7 +545,7 @@ void main()
     	        default:
     	            break;
     	    }
-    	    printf("Do you want to exit? If yes, enter 1. If no, enter 2");
+    	    printf("Do you want to exit? If yes, enter 1. If no, enter 2\n");
     	    int n;
     	    scanf("%d",&n);
     	    if (n==1)
@@ -556,6 +561,7 @@ void main()
     default :
     	printf("Please choose a valid input\n");
     break;
+    }
     	
     update_records();
 }
